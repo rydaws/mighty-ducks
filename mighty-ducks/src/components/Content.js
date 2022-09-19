1fb4eba9d3f23ba081c15e8182123190f7eb575a
madigaudio <112047011+madigaudio@users.noreply.github.com>
2022-09-19 17:37:33 -0400

    Update Content.js

--- mighty-ducks/src/components/Content.js
+++ mighty-ducks/src/components/Content.js
@@ -1,10 +1,8 @@
 export const Content = () => {
     return (
         <main>
-            <ul>
             <p>Content</p>
             <p>Box here</p>
-            </ul>
         </main>
     )
 }
