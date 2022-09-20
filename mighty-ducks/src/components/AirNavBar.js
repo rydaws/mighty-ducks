e966db664fd1f8f03e9774ee925f3f21e4a438e4
Ryan Dawson <rdawson40601@gmail.com>
2022-09-20 19:34:37 -0400

    Added styling to navbar

--- mighty-ducks/src/components/AirNavBar.js
+++ mighty-ducks/src/components/AirNavBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -5,14 +6,20 @@ import Navbar from 'react-bootstrap/Navbar';
 function AirNavBar() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Travel Wizard</Navbar.Brand>
+      <Navbar sticky="top" bg="dark" variant="dark" className="Navbar">
+        <Container className="container">
+          <Navbar.Brand href="#home" className="navbar-brand">
+            <img
+            src="logo192.png"
+            width="30"
+            height="30"
+            alt="Logo"
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#bookings">Bookings</Nav.Link>
-            <Nav.Link href="#history">History</Nav.Link>
+            <Nav.Link href="#about" className="navbar-links">About</Nav.Link>
+            <Nav.Link href="#bookings" className="navbar-links">Bookings</Nav.Link>
+            <Nav.Link href="#history" className="navbar-links">History</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
