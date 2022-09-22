89e7b5ac28804d245dad919a19c4d7114bf4970b
Efeeney07 <89642750+Efeeney07@users.noreply.github.com>
2022-09-22 18:50:17 -0400

    edited about page and node modules for backend

--- mighty-ducks/src/pages/About.js
+++ mighty-ducks/src/pages/About.js
@@ -1,8 +1,40 @@
 import React from "react"
+
 function About() {
     return (
         <section>
-            <p>Yo waddup, its team mason and this website is a project to practice working in teams. This website is as airplane price comparator, we show you the cheapest airplane tickets from multiple different airlines     </p>
+            <div className ="styleAbout">
+                Yo waddup, its team mason and this website is a project to practice working in teams. This website is an airplane price comparator, we show you the cheapest airplane tickets from multiple different airlines 
+            </div>
+            <div className="imageBox">
+                <img src="image0.jpg" alt="img0"/>
+                    
+            </div>
+            <div className="textbox">
+                Eavan Feeney
+            </div>
+            <div className="imageBox2">
+                <img src="image0.jpg" alt="img0"/>
+                    
+            </div>
+            <div className="textbox2">
+                Zach Weller
+            </div>
+            <div className="imageBox3">
+                <img src="image0.jpg" alt="img0"/>
+                    
+            </div>
+            <div className="textbox3">
+                Ryan Dawson
+            </div>
+            <div className="imageBox4">
+                <img src="image0.jpg" alt="img0"/>
+                    
+            </div>
+            <div className="textbox4">
+                Mason Digaudio
+            </div>
+            
             
         </section>
     )
