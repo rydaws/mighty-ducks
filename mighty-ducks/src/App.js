b811cc0bf822f83a74c50066127d4e59f2cea2e4
ZachWeller <112107584+ZachWeller@users.noreply.github.com>
2022-09-22 13:21:59 -0400

    HTML fix
    added different pages for bookings, about, history and home

--- mighty-ducks/src/App.js
+++ mighty-ducks/src/App.js
@@ -1,15 +1,25 @@
 //import './App.css';
 /*import { Header } from './components/Header'*/
 import { Footer } from './components/Footer'
-import { Content } from './components/Content'
+
 import AirNavBar from './components/AirNavBar';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Bookings from './pages/Bookings'
+import History from './pages/History'
 function App() {
   return (
     <div className="App">
       <AirNavBar />
-      <Content />
-      <Footer />
-    
+      <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Home" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/History" element={<History />} />
+            <Route path="/Bookings" element={<Bookings />} />
+      </Routes> 
+      <Footer />    
     </div>
   );
 }
--- mighty-ducks/src/components/AirNavBar.js
+++ mighty-ducks/src/components/AirNavBar.js
@@ -8,7 +8,7 @@ function AirNavBar() {
     <>
       <Navbar expand="md" sticky="top" bg="primary" variant="dark" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home" >
+          <Navbar.Brand href="home" >
             <img
             src="logo192.png"
             width="30"
@@ -19,9 +19,9 @@ function AirNavBar() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#bookings">Bookings</Nav.Link>
-            <Nav.Link href="#history">History</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="bookings">Bookings</Nav.Link>
+            <Nav.Link href="history">History</Nav.Link>
           </Nav>
           </Navbar.Collapse>
         </Container>
