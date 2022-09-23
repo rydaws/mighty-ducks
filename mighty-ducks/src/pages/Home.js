d7c9556f77298db735f163be7a6b034903b4bc04
madigaudio <112047011+madigaudio@users.noreply.github.com>
2022-09-22 22:37:13 -0400

    Added date input bars and button with style

--- mighty-ducks/src/pages/Home.js
+++ mighty-ducks/src/pages/Home.js
@@ -5,9 +5,15 @@ function Home() {
     return (
         <main>
             
-            <div class="searchbar">
-            <input type="text" placeholder="Arriving to..."></input>
-            <input type="text" placeholder="Departing from..."></input>
+            <div class="searchbars">
+                <input type="text" placeholder="Arriving to..."></input>
+                <input type="text" placeholder="Departing from..."></input>
+                <button class="ticketsearch" type="button">Search</button>
+            </div>
+
+            <div class="datebars">
+                <input type="text" placeholder="Return date (MM/DD/YYYY)"></input>
+                <input type="text" placeholder="Leave date (MM/DD/YYYY)"></input>
             </div>
         </main>
         );
