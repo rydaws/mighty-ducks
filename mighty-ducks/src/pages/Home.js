847b3e37111c6212c9a7dc0c85bc10d3fe7212ee
Ryan Dawson <rdawson40601@gmail.com>
2022-09-24 14:42:59 -0400

    Adjusted search element location

--- mighty-ducks/src/pages/Home.js
+++ mighty-ducks/src/pages/Home.js
@@ -1,21 +1,22 @@
-import React from "react"
+import React from "react";
 
 function Home() {
-    console.log("At Home");
-    return (
-        <main>
-            
-            <div class="searchbars">
-                <input type="text" placeholder="Arriving to..."></input>
-                <input type="text" placeholder="Departing from..."></input>
-                <button class="ticketsearch" type="button">Search</button>
-            </div>
+  console.log("At Home");
+  return (
+    <div className="home">
+      <div className="searchbars">
+        <input type="text" placeholder="Departing from..."></input>
+        <input type="text" placeholder="Arriving to..."></input>
+      </div>
 
-            <div class="datebars">
-                <input type="text" placeholder="Return date (MM/DD/YYYY)"></input>
-                <input type="text" placeholder="Departure date (MM/DD/YYYY)"></input>
-            </div>
-        </main>
-        );
-    }
-export default Home
+      <div className="datebars">
+        <input type="text" placeholder="Departure date (MM/DD/YYYY)"></input>
+        <input type="text" placeholder="Return date (MM/DD/YYYY)"></input>
+      </div>
+      <div className="buttonPosition">
+        <button className="ticketsearch" type="button">Search</button>
+      </div>
+    </div>
+  );
+}
+export default Home;
