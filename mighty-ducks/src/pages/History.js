f304f3036c0417870dd0f67e7f630f320099f99a
madigaudio <112047011+madigaudio@users.noreply.github.com>
2022-09-26 14:01:23 -0400

    Added to history page and styled

--- mighty-ducks/src/pages/History.js
+++ mighty-ducks/src/pages/History.js
@@ -2,15 +2,42 @@ import React from "react"
 import { Links } from 'react-router-dom'
 function History() {
     return (
-        <section>History
-            <ul>
-                <li>Last flight</li>
-                <li>Last flight</li>
-                <li>Last flight</li>
-                <li>Last flight</li>
-                
-            </ul>
-
+        <section>
+            <div class="flightHistory">
+                <span class="flightHistoryItem">Airline</span>
+                <span class="flightHistoryItem"><strong>Time of Flights</strong></span>
+                <span class="flightHistoryItem">Flight Length</span>
+                <span class="flightHistoryItem">Ticket Price</span>
+                <button className="ticketLink" type="button">Link to Tickets</button>
+            </div>
+            <div class="flightHistory">
+                <span class="flightHistoryItem">Airline</span>
+                <span class="flightHistoryItem"><strong>Time of Flights</strong></span>
+                <span class="flightHistoryItem">Flight Length</span>
+                <span class="flightHistoryItem">Ticket Price</span>
+                <button className="ticketLink" type="button">Link to Tickets</button>
+            </div>
+            <div class="flightHistory">
+                <span class="flightHistoryItem">Airline</span>
+                <span class="flightHistoryItem"><strong>Time of Flights</strong></span>
+                <span class="flightHistoryItem">Flight Length</span>
+                <span class="flightHistoryItem">Ticket Price</span>
+                <button className="ticketLink" type="button">Link to Tickets</button>
+            </div>
+            <div class="flightHistory">
+                <span class="flightHistoryItem">Airline</span>
+                <span class="flightHistoryItem"><strong>Time of Flights</strong></span>
+                <span class="flightHistoryItem">Flight Length</span>
+                <span class="flightHistoryItem">Ticket Price</span>
+                <button className="ticketLink" type="button">Link to Tickets</button>
+            </div>
+            <div class="flightHistory">
+                <span class="flightHistoryItem">Airline</span>
+                <span class="flightHistoryItem"><strong>Time of Flights</strong></span>
+                <span class="flightHistoryItem">Flight Length</span>
+                <span class="flightHistoryItem">Ticket Price</span>
+                <button className="ticketLink" type="button">Link to Tickets</button>
+            </div>
         </section>
         
     )
