376864fa6bbccdce7b2e7bc03a9ae4575a36ced4
Ryan Dawson <rdawson40601@gmail.com>
2022-09-26 17:07:39 -0400

    Fixed about page positioning

--- mighty-ducks/src/pages/About.js
+++ mighty-ducks/src/pages/About.js
@@ -1,46 +1,43 @@
-import React from "react"
+import React from "react";
 function About() {
-    return (
-        <section>
-                        <div className ="styleAbout">
-                Welcome to Ticket Wizard, the only website you need to find the lowest price for any flight. Here at Ticket Wizard, we want the lowest prices to be 
-                easily accessible to people instead of having to look through multiple different websites to compare. Our team is made up of four students and we are excited to have Ticket
-                Wizard as our first functional web app.  
-            </div>
-            <div className="aboutHead">
-                <p>Meet the Team</p>
-            </div>
-            <div className="imageBox">
-                <img src="eavan.png" alt="imgEavan"/>
+  return (
+    <section>
+      <div className="aboutHeader">
+        <div className="styleAbout">
+          Welcome to Ticket Wizard, the only website you need to find the lowest
+          price for any flight. Here at Ticket Wizard, we want the lowest prices
+          to be easily accessible to people instead of having to look through
+          multiple different websites to compare. Our team is made up of four
+          students and we are excited to have Ticket Wizard as our first
+          functional web app.
+        </div>
+        <div className="meetTeam">
+          <p>Meet the Team</p>
+        </div>
+      </div>
+      <div className="meetTeamInfo">
+        <div className="imageBox">
+          <img src="eavan.png" alt="imgEavan" />
+        </div>
+        <div className="imageBox">
+          <img src="zach.png" alt="imgZach" />
+        </div>
 
-            </div>
-            <div className="textbox">
-                Eavan Feeney
-            </div>
-            <div className="imageBox2">
-                <img src="zach.png" alt="imgZach"/>
+        <div className="imageBox">
+          <img src="ryan.png" alt="imgRyan" />
+        </div>
 
-            </div>
-            <div className="textbox2">
-                Zach Weller
-            </div>
-            <div className="imageBox3">
-                <img src="ryan.png" alt="imgRyan"/>
-
-            </div>
-            <div className="textbox3">
-                Ryan Dawson
-            </div>
-            <div className="imageBox4">
-                <img src="mason.png" alt="imgMason"/>
-
-            </div>
-            <div className="textbox4">
-                Mason Digaudio
-            </div>
-            
-        </section>
-    )
+        <div className="imageBox">
+          <img src="mason.png" alt="imgMason" />
+        </div>
+      </div>
+      <div className="textBoxPosition">
+        <div className="textbox">Eavan Feeney</div>
+        <div className="textbox">Zach Weller</div>
+        <div className="textbox">Ryan Dawson</div>
+        <div className="textbox">Mason Digaudio</div>
+      </div>
+    </section>
+  );
 }
-
-export default About
+export default About;
