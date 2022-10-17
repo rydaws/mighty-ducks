0abcddd6bfa09473f9a638ffe12c8a37612ecc82
madigaudio <112047011+madigaudio@users.noreply.github.com>
2022-10-17 19:11:41 -0400

    Added origin, destination, departDate, returnDate variables

--- mighty-ducks/src/flightAPI.py
+++ mighty-ducks/src/flightAPI.py
@@ -2,7 +2,12 @@ import requests
 
 url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/calendar"
 
-querystring = {"calendar_type":"departure_date","destination":"LAX","origin":"JFK","depart_date":"2022-10-18","currency":"USD","return_date":"2022-10-22"}
+origin = "JFK"
+destination = "LAX"
+departDate = "2022-10-18"
+returnDate = "2022-10-22"
+
+querystring = {"calendar_type":"departure_date","destination":destination,"origin":origin,"depart_date":departDate,"currency":"USD","return_date":returnDate}
 
 headers = {
 	"X-Access-Token": "4eaf2071e912db689ea22d419dd5ecca",
