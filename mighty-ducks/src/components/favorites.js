acc20551dcc95850ca74dc0415e0b89c2a71bc11
Efeeney07 <89642750+Efeeney07@users.noreply.github.com>
2022-10-19 20:41:07 -0400

    created favorites page and created script to create favorites

--- mighty-ducks/src/components/favorites.js
+++ mighty-ducks/src/components/favorites.js
@@ -1,7 +1,27 @@
-import React from "react"
-import Favorite from "../backend/Favorite"
-//import flightSchema from "../backend/models/flightSchema"
-function History() {
+const mongoose = require('mongoose');
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// const Record = (props) => (
+//     <tr>
+//       <td>{props.Favorite.userFrom}</td>
+//       <td>{props.Favorite.Flight}</td>
+//       <td>
+//         <Link className="btn btn-link" to={`/edit/${props.Favorite._id}`}>Edit</Link> |
+//         <button className="btn btn-link"
+//           onClick={() => {
+//             props.deleteRecord(props.record._id);
+//           }}
+//         >
+//           Delete
+//         </button>
+//       </td>
+//     </tr>
+//    );
+
+
+
+function Favorites() {
     return (
         <section>
             <div class="flightHistory">
@@ -9,7 +29,6 @@ function History() {
                 <span class="flightHistoryItem"><strong>10:29 PM</strong></span>
                 <span class="flightHistoryItem">6H 2M</span>
                 <span class="flightHistoryItem">$368</span>
-                <Favorite userFrom={localStorage.getItem('userId')}/>
                 <button className="ticketLink" type="button">Link to Tickets</button>
             </div>
             <div class="flightHistory">
@@ -17,7 +36,6 @@ function History() {
                 <span class="flightHistoryItem"><strong>9:45 PM</strong></span>
                 <span class="flightHistoryItem">13H 50M</span>
                 <span class="flightHistoryItem">$371</span>
-                <Favorite userFrom={localStorage.getItem('userId')}/>
                 <button className="ticketLink" type="button">Link to Tickets</button>
             </div>
             <div class="flightHistory">
@@ -25,7 +43,6 @@ function History() {
                 <span class="flightHistoryItem"><strong>6:30 AM</strong></span>
                 <span class="flightHistoryItem">9H 48M</span>
                 <span class="flightHistoryItem">$422</span>
-                <Favorite userFrom={localStorage.getItem('userId')}/>
                 <button className="ticketLink" type="button">Link to Tickets</button>
             </div>
             <div class="flightHistory">
@@ -33,7 +50,6 @@ function History() {
                 <span class="flightHistoryItem"><strong>7:00 AM</strong></span>
                 <span class="flightHistoryItem">6H 0M</span>
                 <span class="flightHistoryItem">$478</span>
-                <Favorite userFrom={localStorage.getItem('userId')}/> 
                 <button className="ticketLink" type="button">Link to Tickets</button>
             </div>
             <div class="flightHistory">
@@ -41,7 +57,6 @@ function History() {
                 <span class="flightHistoryItem"><strong>10:00 AM</strong></span>
                 <span class="flightHistoryItem">6H 4M</span>
                 <span class="flightHistoryItem">$503</span>
-                <Favorite userFrom={localStorage.getItem('userId')}/>  
                 <button className="ticketLink" type="button">Link to Tickets</button>
             </div>
         </section>
@@ -49,4 +64,4 @@ function History() {
     )
 }
 
-export default History
+export default Favorites
