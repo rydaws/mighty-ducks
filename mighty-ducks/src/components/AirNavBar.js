4677d18f1423cd0fba7b4399f9400f0268bf205a
Ryan Dawson <rdawson40601@gmail.com>
2022-11-02 22:05:40 -0400

    Attempt to locate JSON file, work on validation

--- mighty-ducks/src/components/AirNavBar.js
+++ mighty-ducks/src/components/AirNavBar.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export async function updateNavBar() {
+  document.getElementById("sess").innerHTML = "dfddsss";
+}
 
 function AirNavBar() {
   return (
     <>
-      <Navbar expand="md" sticky="top" bg="primary" variant="dark" collapseOnSelect>
+      <Navbar
+        expand="md"
+        sticky="top"
+        bg="primary"
+        variant="dark"
+        collapseOnSelect
+      >
         <Container>
-          <Navbar.Brand href="home" >
-            <img
-            src="wizard_logo_man.png"
-            width="60"
-            height="39"
-            alt="Logo"
-            />
+          <Navbar.Brand href="/">
+            <img src="wizard_logo_man.png" width="60" height="39" alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="about">About</Nav.Link>     
-            <Nav.Link href="history">History</Nav.Link>
-          </Nav>
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="signup">Signup</Nav.Link>
-            <Nav.Link href="login">Login</Nav.Link>
-          </Nav>
+            <Nav className="mr-auto">
+              <Nav.Link href="about">About</Nav.Link>
+              <Nav.Link href="history">History</Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="signup">Signup</Nav.Link>
+              <Nav.Link href="login">Login</Nav.Link>
+              <Nav.Link href="home">
+                <div id="sess">null</div>
+              </Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
--- mighty-ducks/src/components/Login.js
+++ mighty-ducks/src/components/Login.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "../custom.scss";
 import { useNavigate } from "react-router";
+import { updateNavBar } from "./AirNavBar";
+var loggedIn = false;
 
+
+window.onload = async function update (){
+  updateNavBar();
+  document.getElementById("test").innerHTML = loggedIn;
+}
 export default function Login() {
+  function updateState() {
+    loggedIn = true;
+    document.getElementById("test").innerHTML = loggedIn;
+  }
+  
   const [form, setForm] = useState({
     username: "",
     password: "",
@@ -18,6 +30,7 @@ export default function Login() {
   
   // This function will handle the submission.
   async function onSubmit(e) {
+    
     e.preventDefault();
   
     // When a post request is sent to the create url, we'll add a new record to the database.
@@ -40,40 +53,41 @@ export default function Login() {
 
 
 
-
-
-
-
-
-
-
-
 //-------------------------------------------------------//
-  const [records, setRecords] = useState([]);
-  useEffect(() => {
-    async function getRecords() {
-      const response = await fetch(`http://localhost:3000/record/`);
+  // const [records, setRecords] = useState([]);
   
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
+  // useEffect(() => {
+  //   async function getRecords() {
+  //     const response = await fetch(`http://localhost:3000/record/`);
   
-      const records = await response.json();
-      console.log(records);
-      setRecords(records);
-    }
+  //     if (!response.ok) {
+  //       const message = `An error occurred: ${response.statusText}`;
+  //       window.alert(message);
+  //       return;
+  //     }
   
-    getRecords();
-  
-    return;
-  }, [records.length]);
+  //     const records = await response.json();
+      
+  //     console.log(records);
 
+  //     // this shows that we can use the records array to compare data
+  //     document.getElementById("test").innerHTML = records[0].username
+
+  //     setRecords(records);
+  //   }
+  
+  //   getRecords();
+  
+
+  //   return;
+  // }, [records.length]); 
+  
+  // USE PATTERN FOR PASSWORD STRENGTH
   return (
 <section>
    <div>
      <h3>Login</h3>
+     <div id="test">h</div>
      <form onSubmit={onSubmit}>
        <div className="form-group">
          <label htmlFor="username">Username</label>
@@ -103,6 +117,13 @@ export default function Login() {
            value="Submit"
            className="btn btn-primary"
          />
+       </div>
+       <div>
+        <input
+        type="button"
+        value="state"
+        onClick={updateState}
+        />
        </div>
      </form>
    </div>
