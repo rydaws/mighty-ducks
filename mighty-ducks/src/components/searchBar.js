4352dee081aca3a00c837ae0e066d521082456c6
madigaudio <112047011+madigaudio@users.noreply.github.com>
2022-11-10 20:29:42 -0500

    Added comments to help

--- mighty-ducks/src/components/searchBar.js
+++ mighty-ducks/src/components/searchBar.js
@@ -169,3 +169,79 @@ export async function autocomplete(inp, arr) {
 //         window.location.href = "/Bookings";
 //     }
 // } 
+
+// import React from "react";
+// import 'material-icons/iconfont/material-icons.css'
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+// export default class saveUserInput extends React.Component {
+//     constructor(props) {
+//         super(props);
+
+
+//         this.state = { origin: '' };
+//         this.state = { destination: '' };
+
+//         this.handleOriginChange = this.handleOriginChange.bind(this);
+//         this.handleDestinationChange = this.handleDestinationChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.saveInputs = this.saveInputs.bind(this)
+//     }
+
+//     handleOriginChange(event) {
+//         this.setState({ origin: event.target.value });
+//     }
+
+//     handleDestinationChange(event) {
+//         this.setState({ destination: event.target.value });
+//     }
+
+//     handleSubmit(event) {
+//         event.preventDefault();
+//         console.log(this.state.origin)
+//         console.log(this.state.destination)
+//     }
+
+//     saveInputs() {
+//         var userInputs = {
+//             origin: this.state.origin,
+//             destination: this.state.destination
+//         };
+//         userInputs = JSON.stringify(userInputs);
+//         userInputs = btoa(userInputs);
+//         localStorage.setItem('_userInputs', userInputs);
+//         window.location("/Bookings")
+//     }
+
+
+//     render() {
+
+//         return (
+//             <form onSubmit={this.handleSubmit} method='GET'>
+//                 <div className="container">
+//                     <div className="searchbars">
+//                         <Row>
+//                             <Col>
+//                                 <div className="searchbar-1">
+//                                     <i className="material-icons">search</i><input id="origin" type="text" value={this.state.origin} onChange={this.handleOriginChange} placeholder="Departing From..." method="GET" />
+//                                 </div>
+//                             </Col>
+//                             <Col>
+//                             <div className="searchbar-2">
+//                             <i className="material-icons">search</i><input id="destination" type="text" value={this.state.destination} onChange={this.handleDestinationChange} placeholder="Arriving To..." method="POST" />
+//                             <a href="Bookings">
+//                                 <button className="searchButton" type="button" value="Submit" onClick={this.saveInputs}>Go</button>
+//                             </a>
+//                             </div>
+//                             </Col>
+                           
+//                         </Row>
+//                     </div>
+//                 </div >
+
+//             </form >
+
+//         );
+//     }
+// }
